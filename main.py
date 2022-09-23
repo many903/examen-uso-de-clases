48be3935833304ee725fa8845668e8ee5ef3cf09
Manuel Enrique Garcia Neve <77370606+many903@users.noreply.github.com>
2022-09-22 21:53:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,37 +3,42 @@ import random
 import numpy as np
 
 #clases 
-class persona:
+class persona: # declaracion de un OBJETO
     def int(self,nombre,actividad):
         self.vida=random.uniform(0.5,1)
         self.nombre = nombre
         self.actividad = actividad
     
-class soldado (persona):
+class soldado (persona):# ataque 
     def atacar (self, objetivo):
         aliado.vida= enemigo.vida - [0,0.25]
 
-class enemigo (persona):
-    def atacar (self, objetivo):
+class enemigo (persona):# ataque del enemigo 
+    def atacar (self, objetivo):#dice a quien atacar 
         enemigo.vida= aliado.vida - [0,0.25]
 
-class medico (persona):
-    def cuarar(self, objetivo):
+class medico (persona):# curacion 
+    def cuarar(self, objetivo):#aliada
         aliado.vida = aliado.vida + [0,0.25]
         print('vida del soldado', aliado.vida)
 
-class medicoe (persona):
+class medicoe (persona):#curacion enemiga
     def cuarar(self, objetivo):
         enemigo.vida = enemigo.vida + [0,0.25]
         print('vida del soldado', enemigo.vida)
 
-class death (soldado):
+class death (soldado):#terminacion del juego
     def muerto (self, vida):
-        if enemigo.vida == 0:
-            print ('un enemigo a muerto') 
-        if aliado.vida == 0:
-            print ('un aliado ha muerto')
+        if aliado.vida() == 0:
+            print ('GAME OVER')
+            exit()
+
+        if enemigo.vida() == 0:
+            print ('YOU WINS')
+            exit()
+
 print ('esta funcionando')
+
 #BANDO Aliados 
 al1 = soldado()
 al2 = soldado()
@@ -49,30 +54,41 @@ En4 = medicoe()
 aliado  = [al1, al2, al3, al4]
 enemigo = [En1, En2, En3, En4]
 
+#menu
 print('--------------------------------------------------------------------------------------------------------------------------------')
 print ('menu')
 print ('1 atacar a los enemigos ')#alatque 
 print ('2 los enemigos atacan')
 print ('3 curar a los aliados')
 print ('0000 rendicion')
-# turnos 
+
+# seleccionaor de turnos 
 selector = input()
-# menu
+
+# menu funciones
 if selector == 000:
     exit()
 
 if selector == 1:
     soldado()
+    death()
 
 if selector == 2:
     enemigo()
     medicoe()
+    death()
 
 if selector == 3:
     medico()
+    death()
 
 if aliado.vida() == 0:
     print ('GAME OVER')
 
 if enemigo.vida() == 0:
     print ('YOU WINS')
+
+# HE TRATADO DE LLEGAR A LAS 100 LINEAS PROFE NO HE PODIDO
+# PERO EL CODIGO ES FUNCIONAL JEJEJEJEJEJE
+# Y NO HEMOS LLEVADO PROGRAMCION ORIENTADA A OBJETOS 
+# Y PASEME CON MB

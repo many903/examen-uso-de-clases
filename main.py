2f08098a7ab1f4f308ff4ca9a45238413f941971
Manuel Enrique Garcia Neve <77370606+many903@users.noreply.github.com>
2022-09-22 21:17:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #librerias 
 import random 
+import numpy as np
 
 #clases 
 class persona:
@@ -10,12 +11,14 @@ class persona:
     
 class soldado (persona):
     def atacar (self, objetivo):
-        enemigo.vida= enemigo.vida - [0,0.25]
+        vidae.vida= enemigo.vida - [0,0.25]
 
 class medico (persona):
     def cuarar(self, objetivo):
-        aliado.vida = aliado.vida + [0,0.25]
-
+        vida.vida = aliado.vida + [0,0.25]
+        print('vida del soldado', aliado.vida)
+class actividades(persona):
+    def soldado (self,)
 class death (soldado):
     def muerto (self, vida):
         if enemigo.vida == 0:
@@ -35,6 +38,60 @@ En2 = soldado()
 En3 = soldado()
 En4 = medico()
 
-aliado = [al1, al2, al3, al4]
-enemigo = [En1, En2, En3, En4]
+vida = 0
+vidae= 0
 
+print ('la vida del aliado es:', al1)
+print ('la vida del aliado es:', al2)
+print ('la vida del aliado es:', al3)
+print ('la vida del medico aliado es:', al4)
+print ('la vida del enemigo es:', En1)
+print ('la vida del enemigo es:', En2)
+print ('la vida del enemigo es:', En3)
+print ('la vida del meico enemigo es:', En4
+
+print ('menu')
+print ('1')#alatque 
+print ('2')
+print ('3')
+print ('4')
+print ('5')
+print ('6')
+print ('7')
+print ('8')
+print ('9')
+print ('10')
+print ('0000')
+
+if selector == 000:
+    soldados2 = soldados2 + medicos
+
+if selector == 1:
+    soldados2 = soldados2 + medicos
+
+if selector == 2:
+    soldados = soldados - guerra 
+
+if selector == 3:
+    soldados2 = soldados2 - guerra
+
+if selector == 4:
+    soldados = soldados + medicos 
+
+if selector == 5:
+    soldados2 = soldados2 + medicos
+
+if selector == 6:
+    soldados2 = soldados2 + medicos
+
+if selector == 7:
+    soldados2 = soldados2 + medicos
+
+if selector == 8:
+    soldados2 = soldados2 + medicos
+
+if selector == 9:
+    soldados2 = soldados2 + medicos
+
+if selector == 10:
+    soldados2 = soldados2 + medicos
